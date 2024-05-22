7c0d207b9cfb5a28af58f496c5859754da47040c
anup-panchal <anup@7span.com>
2024-05-22 17:51:19 +0530

    Add Assertion example

--- src/main/java/com/juint5/assertion/AssertionExample.java
+++ src/main/java/com/juint5/assertion/AssertionExample.java
@@ -1,9 +1,25 @@
 package com.juint5.assertion;
 
 public class AssertionExample {
+
     public static void main(String[] args) {
-        int age = -5;
-        assert age >= 0 : "Age cannot be negative: " + age;
-        System.out.println("Age is " + age);
+        // Enable assertions using -ea flag when running the program
+        // Example: java -ea AssertionExample
+
+        int result = factorial(5);
+        System.out.println("Factorial of 5 is: " + result);
+
+        // This will trigger an assertion error
+        result = factorial(-1);
+    }
+
+    public static int factorial(int n) {
+        assert n >= 0 : "The input number must be non-negative";
+        // Basic factorial calculation
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
     }
 }
